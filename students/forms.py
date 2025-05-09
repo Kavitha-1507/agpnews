7422d395548b0de539820bcbb500d9407b892c59
Kavitha-1507 <kskavitha86303@gmail.com>
2025-05-09 11:08:02 +0530

    first commit

---
+++ students/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from students.models import Student
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        # fields = "__all__"
+        # exclude = ["name","roll_no"]
+        # fields = ["name", "roll_no", "mobile", "extra_field"]
+        fields =["name", "roll_no", "mobile"]
+
+    def clean_mobile(self):
+        print(self.cleaned_data)
+        mobile = self.cleaned_data.get("mobile")
+        if len(mobile)<10:
+            raise forms.ValidationError("Mobile number should be exactly 10 digits")
+        return mobile
